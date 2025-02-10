bed8b6c9af4e8ab0c459d3e00fb9dcea6f3734ec
Sumit Sontakke <sms@Supriyas-MacBook-Pro.local>
2025-02-10 10:02:34 +0530

    configuring terraform into github actions_1

--- nothing.tf
+++ nothing.tf
@@ -1,5 +1,5 @@
 # An example resource that does nothing.
-resource "null_resource" "example" {
+resource "null_resource0" "example" {
   triggers = {
     value = "A example resource that does nothing!"
   }

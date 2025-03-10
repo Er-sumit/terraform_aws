c3815ef8b8da273b475ac2f7cfecd86f4f083584
Sumit Sontakke <sms@Supriyas-MacBook-Pro.local>
2025-03-10 22:50:35 +0530

    conditional role creation for ecs

--- 01_service_linked_roles_ecs.tf
+++ 01_service_linked_roles_ecs.tf
@@ -5,6 +5,19 @@ resource "aws_iam_service_linked_role" "elb" {
 }
 
 # Service-Linked Role for ECS
+# resource "aws_iam_service_linked_role" "ecs" {
+#   aws_service_name = "ecs.amazonaws.com"
+# }
+#  - Do NOT directly create this role, first check if this exists and create only if it doesn't
+# Check if the service-linked role for ECS already exists
+data "aws_iam_role" "ecs" {
+  name = "AWSServiceRoleForECS"
+}
+
+# Create the service-linked role for ECS only if it doesn't exist
 resource "aws_iam_service_linked_role" "ecs" {
   aws_service_name = "ecs.amazonaws.com"
+
+  # Create only if the role is not found
+  count = length(data.aws_iam_role.ecs.arn) > 0 ? 0 : 1
 }
